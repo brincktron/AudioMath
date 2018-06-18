cd7b384e0d6d592f853069ccc731f92425654541
brincktron <brincktron@gmail.com>
2018-06-18 18:51:38 +0200

    saves soundcheck style file

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sf
 import scipy.signal
-
 path1 = "DutSpkPinkWav_mic1.wav"
 sig1, fs1 = sf.read(path1)
 
@@ -15,7 +14,14 @@ Cxy_pct = Cxy * 100
 # plt.show()
 
 Cxy_dB = -10 * np.log10(1 - (Cxy))
-np.savetxt('Mic1Mic2_CBD.txt', np.transpose(np.array([f, Cxy_dB])), fmt='%1.3f')
+
+
+soundcheck_info=np.zeros(len(f), dtype='|S8')
+soundcheck_info[0:5]=['Mic1Mic2_CBD', 'x data lin', 'y data lin', 'x axis log', 'y axis lin']
+
+out=np.column_stack([soundcheck_info,f,Cxy_dB])
+np.savetxt('Mic1Mic2_CBD.txt', out, delimiter='\t', fmt='%s')
+
 figure2 = plt.semilogx(f, Cxy_dB)  # type: float
 plt.xlim([100, 8000])
 plt.show(figure2)
